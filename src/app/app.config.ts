aa122ebf71a2b1c243b17dae28c5d2aef91ef125
jandres-vega <jandresvega@itsa.edu.co>
2024-10-17 12:51:44 -0500

    Refactor app.config.ts to import additional modules and update providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {OverlayModule} from '@angular/cdk/overlay';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(OverlayModule),
+    importProvidersFrom(CdkAccordionModule),
+    provideRouter(routes), provideClientHydration()]
 };
