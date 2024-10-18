665d4b929009f811f5633edfac697f73b4cdd53d
jandres-vega <jandresvega@itsa.edu.co>
2024-10-18 15:50:42 -0500

    Refactor app.config.ts to import additional modules and update providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,19 @@ import {OverlayModule} from '@angular/cdk/overlay';
 import {CdkAccordionModule} from '@angular/cdk/accordion';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {DialogModule} from '@angular/cdk/dialog';
+import {provideHttpClient} from '@angular/common/http';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     importProvidersFrom(OverlayModule),
     importProvidersFrom(CdkAccordionModule),
+    importProvidersFrom(DragDropModule),
+    importProvidersFrom(ScrollingModule),
+    importProvidersFrom(DialogModule),
+    provideHttpClient(),
     provideRouter(routes), provideClientHydration()]
 };
