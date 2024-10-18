ff5a402d8cbd605e2b5adbb116d290e527f7d231
jandres-vega <jandresvega@itsa.edu.co>
2024-10-18 15:51:13 -0500

    Add new routes for board and scroll components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {BoardsComponent} from './pages/boards/boards.component';
+import {BoardComponent} from './pages/board/board.component';
+import {ScrollComponent} from './pages/scroll/scroll.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'boards',
     component: BoardsComponent
+  },
+  {
+    path: 'board',
+    component: BoardComponent
+  },
+  {
+    path: 'scroll',
+    component: ScrollComponent
   }
 ];
